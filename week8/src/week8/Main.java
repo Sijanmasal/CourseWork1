11c85a2d940db5e4314b8c33882ba08d4da98edc
Sijanmasal <smasal703@gmail.com>
2021-04-22 11:38:42 +0545

    coursework1

---
+++ week8/src/week8/Main.java
@@ -0,0 +1,14 @@
+package week8;
+
+ 
+
+import week8.swing.LoginUI;
+
+ 
+
+public class Main {
+    public static void main(String[] args) {
+        new LoginUI();
+    }
+}
+ 
---
+++ week8/src/week8/Utils/Registration.java
@@ -0,0 +1,13 @@
+package week8.Utils;
+ 
+import week8.Constants;
+import week8.FileHandler;
+ 
+public class Registration {
+ 
+    public String registerUser(String[] userDetail) {
+        FileHandler fh = new FileHandler();
+        return fh.CreateOrAdd(Constants.userDetailFileName, userDetail);
+    }
+}
+
